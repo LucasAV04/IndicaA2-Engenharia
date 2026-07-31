eb4dcafb5b06b09d0afc504770eaf3a97216f59d
LucasAV04 <Lucasaderbal.2004@gmail.com>
2026-07-31 11:42:35 -0300

    Adicionado a base das entidades de dominio

---
+++ Domain/Entities/BaseEntity.cs
@@ -0,0 +1,23 @@
+﻿namespace Domain.Entities
+{
+    public abstract class BaseEntity
+    {
+        public Guid Id { get; protected set; }
+
+        public DateTime CreatedAt { get; protected set; }
+
+        public DateTime UpdatedAt { get; protected set; }
+
+        protected BaseEntity()
+        {
+            Id = Guid.NewGuid();
+            CreatedAt = DateTime.UtcNow;
+            UpdatedAt = DateTime.UtcNow;
+        }
+
+        public void AtualizarDataAlteracao()
+        {
+            UpdatedAt = DateTime.UtcNow;
+        }
+    }
+}
---
+++ Domain/Enums/TipoUsuario.cs
@@ -0,0 +1,8 @@
+﻿namespace Domain.Enums
+{
+    public enum TipoUsuario
+    {
+        Usuario = 1,
+        Administrador = 2
+    }
+}
