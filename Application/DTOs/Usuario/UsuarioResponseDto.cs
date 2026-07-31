17d69a01589f63f342d2e8db87a159d59a9a8331
LucasAV04 <Lucasaderbal.2004@gmail.com>
2026-07-31 14:26:35 -0300

    Adicionado a parte na camada da Application

---
+++ Application/DTOs/Usuario/UsuarioResponseDto.cs
@@ -0,0 +1,25 @@
+﻿using Domain.Enums;
+
+namespace Application.DTOs.Usuario
+{
+    public sealed class UsuarioResponseDto
+    {
+        public Guid Id { get; set; }
+
+        public string Nome { get; set; } = string.Empty;
+
+        public string Email { get; set; } = string.Empty;
+
+        public string? Telefone { get; set; }
+
+        public StatusUsuario Status { get; set; }
+
+        public TipoUsuario TipoUsuario { get; set; }
+
+        public bool EmailConfirmado { get; set; }
+
+        public DateTime CreatedAt { get; set; }
+
+        public DateTime UpdatedAt { get; set; }
+    }
+}
---
+++ Application/Mapping/UsuarioMapper.cs
@@ -0,0 +1,12 @@
+﻿using Application.DTOs.Usuario;
+using Domain.Entities;
+namespace Application.Mapping
+{
+    public static class UsuarioMApper
+    {
+        public static UsuarioResponseDto ToResponseDto(this Usuario usuario)
+        {
+           
+        }
+    }
+}
