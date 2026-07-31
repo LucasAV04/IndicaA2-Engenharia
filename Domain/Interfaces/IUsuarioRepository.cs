656a90512a7cd102f08dc481c02f9afae9f4f1a6
LucasAV04 <Lucasaderbal.2004@gmail.com>
2026-07-31 13:31:20 -0300

    Adicionado a interface do dominio

---
+++ Domain/Interfaces/IUsuarioRepository.cs
@@ -0,0 +1,33 @@
+﻿using Domain.Entities;
+
+namespace Domain.Interfaces
+{
+    public interface IUsuarioRepository
+    {
+        #region Consultas
+
+        Task<Usuario?> ObterPorIdAsync(Guid id);
+
+        Task<Usuario?> ObterPorEmailAsync(string email);
+
+        Task<Usuario?> ObterPorCodigoIndicacaoAsync(string codigo);
+
+        Task<IReadOnlyCollection<Usuario>> ObterTodosAsync();
+
+        Task<bool> ExistePorIdAsync(Guid id);
+
+        Task<bool> ExistePorEmailAsync(string email);
+
+        #endregion
+
+        #region Comandos
+
+        Task AdicionarAsync(Usuario usuario);
+
+        Task AtualizarAsync(Usuario usuario);
+
+        Task RemoverAsync(Usuario usuario);
+
+        #endregion
+    }
+}
