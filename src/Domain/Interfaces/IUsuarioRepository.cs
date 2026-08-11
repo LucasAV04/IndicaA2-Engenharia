5e2d1dff048ef559af697fa463d73afec838a17c
LucasAV04 <Lucasaderbal.2004@gmail.com>
2026-08-11 11:27:10 -0300

    refactor(usuarios): consolida contrato do repository

--- src/Domain/Interfaces/IUsuarioRepository.cs
+++ src/Domain/Interfaces/IUsuarioRepository.cs
@@ -10,13 +10,10 @@ namespace Domain.Interfaces
 
         Task<bool> ExistePorEmailAsync(string email, Guid? ignorarUsuarioId = null, CancellationToken cancellationToken = default);
 
-        Task<Usuario?> ObterPorCodigoIndicacaoAsync(string codigo);
-
         Task<IReadOnlyCollection<Usuario>> ObterTodosAsync(CancellationToken cancellationToken = default);
 
         Task<bool> ExistePorIdAsync(Guid id);
 
-        Task<bool> ExistePorEmailAsync(string email,CancellationToken cancellationToken = default);
 
         #endregion
 
@@ -25,8 +22,6 @@ namespace Domain.Interfaces
         Task AdicionarAsync(Usuario usuario, CancellationToken cancellationToken = default);
 
         Task AtualizarAsync(Usuario usuario, CancellationToken cancellationToken = default);
-
-        Task RemoverAsync(Usuario usuario, CancellationToken cancellationToken = default);
 
         #endregion
     }
